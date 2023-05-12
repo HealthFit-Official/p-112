06471fe28b4ea671e695713384d0c69e1aa6658a
HealthFit.org <117991750+HealthFit-Official@users.noreply.github.com>
2023-05-12 17:12:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+//Here is the teachable machine link :- https://teachablemachine.withgoogle.com/models/KfOpagDDD/;
+
+Webcam.set({
+    width:400,
+    height:350,
+    image_format : 'png',
+    png_quality:144
+});
+  camera = document.getElementById("camera");
+
+  Webcam.attach('#camera');
+
+  function take_snapshot()
+  {
+     Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML ='<img id="captured_image" src= "'+ data_uri+'"/>';
+     });
+  }
+  console.log('ml5 version:',ml5.version);
+  classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/KfOpagDDD/model.json",modelLoaded);
+
+  function modelLoaded() {
+   console.log('Model Loaded!')
+  }
+
+  function speak(){
+   var synth = window.speechSynthesis;
+   speak_data = "The prediction is"+ perdiction;
+   var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+  }
+
+  function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error){
+        console.error(error);
+    } else{
+        console.log(results);
+        document.getElementById("result_g_name").innerHTML = results[0].label;
+        prediction = results[0].label;
+        speak();
+        if(results[0].label == "thumbs up")
+        {
+            document.getElementById("update_g").innerHTML = "&#128077;";
+        }
+        if(results[0].label == "peace sign")
+        {
+            document.getElementById("update_g").innerHTML = "&#9996;";
+        }
+        if(results[0].label == "nice hand gesture")
+        {
+            document.getElementById("update_g").innerHTML = "&#128076;";
+        }
+       
+    }
+}
+
+
+
